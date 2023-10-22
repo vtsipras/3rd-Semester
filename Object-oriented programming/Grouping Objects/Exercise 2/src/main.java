cfd95f11211c2a984045af238d7d09d1e0a3d2a7
Evangelos Tsipras <euangelostsipras@gmail.com>
2023-10-22 18:52:30 +0300

    2nd Try

--- Object-oriented programming/Grouping Objects/Exercise 2/src/main.java
+++ Object-oriented programming/Grouping Objects/Exercise 2/src/main.java
@@ -1,13 +1,11 @@
-package src;
-
 public class main {
     public static void Main(String[] args){
-        src.subject s1 = new src.subject(1, "data structures", 3);
-        src.subject s2 = new src.subject(2, "object oriented programming", 3);
-        src.subject s3 = new src.subject(3, "basic principles of programming", 1);
+        subject s1 = new subject(1, "data structures", 3);
+        subject s2 = new subject(2, "object oriented programming", 3);
+        subject s3 = new subject(3, "basic principles of programming", 1);
 
     }
-    src.program p = new src.program();
+    program p = new program();
     p.addSub(s1);
     p.addSub(s2);
     p.addSub(s3);
--- Object-oriented programming/Grouping Objects/Exercise 2/src/subject.java
+++ Object-oriented programming/Grouping Objects/Exercise 2/src/subject.java
@@ -1,5 +1,3 @@
-package src;
-
 public class subject {
     private int code;
     private String name;
